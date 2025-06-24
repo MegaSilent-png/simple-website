66a56878253fabf2566ecfde26ff0c35cf66cc76
Mega <luismanuelfontan@gmail.com>
2025-06-23 23:29:35 -0400

    Added comments to show my pain of realizing I dont remember java script ;-;

--- sum.js
+++ sum.js
@@ -1,3 +1,5 @@
+// This took me so much longer than it should be
+// God I forgot so much about javascript
 function addFunction() {
     let p1 = parseInt(document.getElementById("fnum").value);
     let p2 = parseInt(document.getElementById("snum").value);
